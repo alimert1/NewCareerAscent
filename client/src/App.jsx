70de21550f213b74788e09212b0c29b69020b683
alimert1 <alimertyilmazsoftware@gmail.com>
2023-11-16 02:58:07 +0300

    react-router-dom build

---
+++ client/src/App.jsx
@@ -0,0 +1,70 @@
+import { Outlet, Navigate, Route, Routes, useLocation } from 'react-router-dom';
+import { Footer, NavBar } from "./components";
+import {
+  About,
+  AuthPage,
+  Companies,
+  CompanyProfile,
+  FindJobs,
+  JobDetail,
+  UploadJob,
+  UserProfile,
+} from "./pages";
+
+
+
+function Layout() {
+
+  const user = true;
+  const location = useLocation();
+
+  return user?.token ? (
+    <Outlet />
+  ) :
+    (<Navigate to='user-auth' state={{ from: location }} replace />
+    );
+}
+
+
+function App() {
+  
+  const user = {};
+
+  return (
+    <main className='bg-[#f7fdfd]'>
+      <NavBar />
+
+      <Routes>
+        <Route element={<Layout />}>
+
+          <Route path='/' element={<Navigate to='find-jobs' replace={true} />} />
+          <Route path='/find-jobs' element={<FindJobs />} />
+          <Route path='/companies' element={<Companies />} />
+
+          <Route
+            path={
+              user?.user?.accountType === "seeker"
+                ? "/user-profile"
+                : "/user-profile/:id"
+            }
+            element={<UserProfile />}
+          />
+
+          <Route path={"/company-profile"} element={<CompanyProfile />} />
+          <Route path={"/company-profile/:id"} element={<CompanyProfile />} />
+          <Route path={"/upload-job"} element={<UploadJob />} />
+          <Route path={"/job-detail/:id"} element={<JobDetail />} />
+
+        </Route>
+        <Route path='/about-us' element={<About />} />
+        <Route path='/user-auth' element={<AuthPage />} />
+      </Routes>
+
+
+
+      {user && <Footer />}
+    </main>
+  );
+}
+
+export default App;
