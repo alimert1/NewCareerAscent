52f8b87e0f9a1f28ccb24251e39ccc79997fa7f4
alimert1 <alimertyilmazsoftware@gmail.com>
2023-11-21 13:27:57 +0300

    sign up page commit

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,6 @@
 import NavBar from "./NavBar";
 import Footer from "./Footer";
 import CustomButton from "./CustomButton";
-
-export {NavBar, Footer, CustomButton};
+import TextInput from "./TextInput";
+import SignUp from "./SignUp";
+export {NavBar, Footer, CustomButton, TextInput, SignUp};
