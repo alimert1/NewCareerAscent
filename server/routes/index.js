215cf39b2bcc55e801232df7690f1b3e9750015d
alimert1 <alimertyilmazsoftware@gmail.com>
2024-01-03 01:34:24 +0300

    added companies rule

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,7 @@ import express from "express";
 
 import authRoute from "./authRoutes.js";
 import userRoute from "./userRoutes.js";
+import companyRoute from "./companiesRoutes.js";
 
 
 const router = express.Router();
@@ -10,6 +11,7 @@ const path = "/api-v1/";
 
 router.use(`${path}auth`, authRoute); //api-v1/auth/
 router.use(`${path}users`, userRoute); 
+router.use(`${path}companies`, companyRoute); 
 
 
 export default router;
